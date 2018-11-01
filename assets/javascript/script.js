a1df4ca18c494395a84babd935467893d3699146
unknown <arthurclaytonproductions@gmail.com>
2018-11-01 04:28:22 -0700

    need arrows on picture scrole to stay in play in the section and be covered when menu animation is ran

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
 var overlay = document.getElementById('overlay');
 var closeMenu = document.getElementById('close-menu');
-
+var sliderImages = document.querySelectorAll('.slide');
+var arrowLeft = document.querySelector('#arrow-left');
+var arrowRight = document.querySelector('#arrow-right');
+current = 0;
+//nav button icon
 document.getElementById('open-menu').addEventListener('click', function(){
     overlay.classList.add('show-menu')
     removeRemoveMenu()
@@ -17,3 +21,44 @@ function removeShowMenu(){
 function removeRemoveMenu(){
     overlay.classList.remove('remove-menu')
    };
+
+
+//slide images
+function reset(){
+    for(var i = 0; i < sliderImages.length; i++){
+        sliderImages[i].style.display ='none';
+    }
+}
+
+function startSlide(){
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+
+function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+
+function slideRight(){
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current ++;
+}
+arrowLeft.addEventListener('click', function(){
+    if(current === 0){
+        current = sliderImages.length;
+    }
+    slideLeft();
+})
+
+arrowRight.addEventListener('click', function(){
+    if(current === sliderImages.length - 1){
+        current = -1;
+    }
+    slideRight();
+})
+
+startSlide();
