e16144e455168c807f15bee7ee0b5bfd8d146ddd
unknown <arthurclaytonproductions@gmail.com>
2018-10-31 23:01:48 -0700

    fixed the hamburger toggle

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,8 +3,17 @@ var closeMenu = document.getElementById('close-menu');
 
 document.getElementById('open-menu').addEventListener('click', function(){
     overlay.classList.add('show-menu')
+    removeRemoveMenu()
 });
 document.getElementById('close-menu').addEventListener('click', function(){
     overlay.classList.add('remove-menu')
+    removeShowMenu();
 });
 
+function removeShowMenu(){
+ overlay.classList.remove('show-menu')
+};
+
+function removeRemoveMenu(){
+    overlay.classList.remove('remove-menu')
+   };
